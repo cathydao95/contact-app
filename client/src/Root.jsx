74f6149d40246d258f80f60f0a432aab2f23ab4f
Cathy Dao <cathydao95@gmail.com>
2023-09-25 21:35:31 -0700

    add root component

---
+++ client/src/Root.jsx
@@ -0,0 +1,11 @@
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div></div>
+      </div>
+      <div id="detail"></div>
+    </>
+  );
+}

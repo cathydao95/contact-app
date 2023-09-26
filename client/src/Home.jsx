470aed0ce7edfac9e23f7c633288a1dbc9219de2
Cathy Dao <cathydao95@gmail.com>
2023-09-25 22:50:49 -0700

    link to new route

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Contacts from "./Contacts";
 const Home = () => {
   return (
     <div>
       <nav className="navbar">
         <h1 className="title">All Contacts</h1>
-        <button className="addBtn">+</button>
+        <Link to="/create">+</Link>
       </nav>
       <div className="block"></div>
       <div>

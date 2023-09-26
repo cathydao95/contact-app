2d9908bde9e5f56754105fabdd24a172dfdac04b
Cathy Dao <cathydao95@gmail.com>
2023-09-25 21:34:47 -0700

    create contacts coponent and fetch contacts

--- client/src/Contacts.jsx
+++ client/src/Contacts.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const [contacts, setContacts] = useState();
+  const getContacts = async () => {
+    let results = await fetch("http://localhost:8080/api/v1/contacts");
+    let {
+      data: { contacts },
+    } = await results.json();
+    setContacts(contacts);
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  return (
+    contacts && (
+      <div>
+        {contacts.map((contact) => {
+          const { id, name, email, phone, notes } = contact;
+          return (
+            <div key={id} className="contactRow">
+              <Link to={`/${id}`}>{name}</Link>
+            </div>
+          );
+        })}
+      </div>
+    )
+  );
 };
 
 export default Contacts;
